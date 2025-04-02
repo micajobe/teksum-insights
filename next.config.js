9b79113dd18a9c915dbf345a1fb97a8da2c513f8
Micah Slavens <micahslavens@Mac.localdomain>
2025-04-02 17:04:51 -0600

    Fix cheerio dependency and build issues

--- next.config.js
+++ next.config.js
@@ -2,6 +2,17 @@
 const nextConfig = {
   images: {
     unoptimized: true
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+      };
+    }
+    return config;
   }
 };
 
